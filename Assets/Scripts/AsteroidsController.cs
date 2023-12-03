165c4424709846cb9b4e942a3daca2325db3bb10
Sercan <ssercanozerr@gmail.com>
2023-12-03 19:13:26 +0300

    asteroid spawner is added

---
+++ Assets/Scripts/AsteroidsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float rotationSpeed;
+    public float speed;
+    public GameObject explosionAsteroid;
+    public GameObject explosionPlayer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * rotationSpeed;
+        rb.velocity = transform.forward * speed;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bolt")
+        {
+            Instantiate(explosionAsteroid, transform.position, transform.rotation);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        if(other.gameObject.tag == "PlayerShip")
+        {
+            Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject asteroid;
+    public int asteroidCount;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+    IEnumerator AsteroidSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < asteroidCount; i++)
+            {
+                Vector3 spawnLocation = new Vector3(Random.Range(-3, 4), 0, 10);
+                Instantiate(asteroid, spawnLocation, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(AsteroidSpawner());
+    }
+}
