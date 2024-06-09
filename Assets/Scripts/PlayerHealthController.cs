51690990346cfbdb2b68f6bc0ae000024dc28cc1
Sercan <ssercanozerr@gmail.com>
2024-06-09 23:12:21 +0300

    new branch for before object pooling

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -46,7 +46,7 @@ public class PlayerHealthController : MonoBehaviour
 
     IEnumerator LoadScene()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
         SceneManager.LoadSceneAsync(2);
     }
 }
